7908549d7ad9152d02a0ae2b8a10b37ce6ed8334
Sergey <serezha.karpunin@gmail.com>
2017-01-24 15:52:22 +0400

    added closing form by click on background

--- src/main/webapp/js/header_functions.js
+++ src/main/webapp/js/header_functions.js
@@ -1,4 +1,15 @@
 $(function () {
+    $("#search_box_form").submit(function(e){
+        if($('#search_box').val()==''){
+            e.preventDefault();
+        }
+    });
+
+    $(".tags button").click(function () {
+        $("#search_box").val($(this).val());
+        $('#search_box_form').submit();
+    });
+
     $('#call_login_menu_button').click(function () {
         $('#login_menu').fadeToggle(200);
         $('#auth_menu_arrow').fadeToggle(200);
@@ -11,10 +22,14 @@ $(function () {
         }
     });
 
-
     $('#auth_header_call_options_menu_button').click(function () {
         $('#options_menu').fadeToggle(200);
         $('#options_menu_arrow').fadeToggle(200);
+    });
+
+    $('#auth_header_ask_question_button').click(function () {
+        $('#mask').fadeIn(250);
+        $('#question_form_wrapper').fadeIn(250);
     });
 
     $('#question_form').submit(function () {
@@ -24,45 +39,29 @@ $(function () {
         }
     });
 
-    $('#auth_header_ask_question_button').click(function () {
-        $('#mask').fadeIn(250);
-        $('#question_form_wrapper').fadeIn(250);
-    });
-
-    $('#question_form_close_button').click(function () {
-        $('#mask').fadeOut(250);
-        $('#question_form_wrapper').fadeOut(250);
-    });
-
-    $("#search_box_form").submit(function(e){
-        if($('#search_box').val()==''){
-            e.preventDefault();
-        }
-    });
-
-    $(".tags button").click(function () {
-        $("#search_box").val($(this).val());
-        $('#search_box_form').submit();
-    });
-
     $('.delete_question_button').click(function () {
         $('#mask').fadeIn(250);
         $('#confirm_question_deletion_form_wrapper').fadeIn(250);
         document.cookie = "id_question_to_delete=" + $(this).val() + "; path=/;";
     });
 
-
-    $('.confirm_deletion_close_button').click(function () {
+    var closeForm = function(){
         $('#mask').fadeOut(250);
+        $('#question_form_wrapper').fadeOut(250);
         $('#confirm_question_deletion_form_wrapper').fadeOut(250);
         $('#confirm_answer_deletion_form_wrapper').fadeOut(250);
         delete_cookie("id_answer_to_delete");
         delete_cookie("id_question_to_delete");
-    })
+    };
+
+    $('#question_form_close_button').click(closeForm);
+    $('.confirm_deletion_close_button').click(closeForm);
+    $('#mask').click(closeForm);
+
 });
 
-function delete_cookie ( cookie_name )
-{
+
+function delete_cookie ( cookie_name ) {
     var cookie_date = new Date ( );  // Текущая дата и время
     cookie_date.setTime ( cookie_date.getTime() - 1 );
     document.cookie = cookie_name + "=; expires=" + cookie_date.toGMTString() + "; path=/;";
